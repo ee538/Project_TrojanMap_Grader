2f88e1e502bb6c59603cec19e853bbdad2d53fc8
Yijun Liu <yijunl@usc.edu>
2022-07-03 16:46:35 -0700

    Update coding_grades_total_ph1.py

--- coding_grades_total_ph1.py
+++ coding_grades_total_ph1.py
@@ -9,11 +9,11 @@ for q_num in q_nums:
 	pass_num = get_ok_num_perq("grades/Q%sres_.txt" % q_num)
 
 	if q_num == 1:
-		q1_score = pass_num * 2.5
+		q1_score = pass_num * 0.5
 		print("Item 1:", pass_num, "/", 2, "passed | score:", q1_score)
 		total_coding_score += q1_score
 	elif q_num == 2.1:
-		q2_score = pass_num * 5
+		q2_score = pass_num * 1
 		print("Item 2-1:", pass_num, "/", 1, "passed | score:", q2_score)
 		total_coding_score += q2_score
 	else:
